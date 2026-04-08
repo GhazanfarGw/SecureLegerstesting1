5bd20986256acd495d5d55ce6ba097e551c54d9f
Ghazanfar Abbas <ghazanfargwi@gmail.com>
2026-04-08 05:54:22 +0500

    Update Footer.jsx

--- src/Home/Footer.jsx
+++ src/Home/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className='bg-[#252E42]'>
             <div className='max-w-screen-xl mx-auto justify-center md:pt-20 md:pb-10 md:px-10 py-10'>
                 <h1 className='text-[#aaa] md:text-base text-sm justify-center pb-5 px-5 '>
-                    Website last updated: March 10, 2026
+                    Website last updated: April 08, 2026
                 </h1>
                 <span className="py-2 md:px-0 px-2">
                     <a href="/">
